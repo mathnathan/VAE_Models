acde1f81fb2fa2b8a5eb5c3f131f2a7f1d2e072a
Nathan Crock <mathnathan@gmail.com>
2017-08-08 16:15:40 -0400

    removed personal logging and plots. Relying on tensorboard now

--- test_drive.py
+++ test_drive.py
@@ -43,44 +43,11 @@ network = model(input_dim, encoder, latency_dim, decoder, hyperParams)
 
 itrs_per_epoch = mnist.train.num_examples // hyperParams['batch_size']
 epochs = 100
-updates = 1000
-cost = 0
-reconstruct_cost = 0
-kl_cost = 0
-winSize = 28
-filename = 'figures'
 
-#for itr in tqdm(range(epochs*itrs_per_epoch)):
-nx = ny = 20
-winRange = 10
-canvas = np.empty((winSize*ny,winSize*nx))
-numPlots = 0
-x_values = np.linspace(-winRange, winRange, nx)
-y_values = np.linspace(-winRange, winRange, ny)
-interval = 1000 # save fig of latency space every 10 batches
-
-img_cost = 0.0
-test_data, test_labels = mnist.test.next_batch(1000)
-test_labels = [np.where(arr == 1) for arr in test_labels]
+test_data, test_labels = mnist.train.next_batch(hyperParams['batch_size'])
 for itr in tqdm(range(epochs*itrs_per_epoch)):
     train_data, train_labels = mnist.train.next_batch(hyperParams['batch_size'])
     tot_cost, reconstr_loss, KL_loss = network(train_data)
-    cost += tot_cost
-    img_cost += tot_cost
-    reconstruct_cost += reconstr_loss
-    kl_cost += KL_loss
-    if itr % updates == 0:
-        c = cost/(updates*hyperParams['batch_size'])
-        rl = reconstruct_cost/(updates*hyperParams['batch_size'])
-        kl = kl_cost/(updates*hyperParams['batch_size'])
-        print("\nrecon_loss=%f, KL_loss=%f, cost=%f" % (rl,kl,c))
-        cost = 0
-        reconstruct_cost = 0
-        kl_cost = 0
-        if hasattr(network, 'get_gmm_params'):
-            pi, mean, std = network.get_gmm_params()
-            print("Modes\n", pi)
-            print("Means\n", mean)
 
 test_data, test_labels = mnist.test.next_batch(1000)
 network.create_embedding(test_data, np.where(test_labels)[1])
