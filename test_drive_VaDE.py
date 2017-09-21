4ff71d00f00f2379e2f75a95efbbff6c7e78caa3
Nathan Crock <mathnathan@gmail.com>
2017-09-21 17:30:58 -0400

    forgot the driver to test the gmm prior

---
+++ test_drive_VaDE.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+from IPython import embed
+import sys
+from sklearn.manifold import TSNE
+
+# Choose standard VAE or VaDE
+#from VAE_Models.VaDE import VaDE as model
+from VAE_Models.VAE import VAE as model
+from VAE_Models.architectures import DNN
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+#from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+#print_tensors_in_checkpoint_file(file_name='logs/embed.ckpt-0', tensor_name='Create_Embedding/Latent_Space', all_tensors=False)
+#sys.exit()
+
+# ---- First Estimate the GMM parameters using standard
+# ---- autoencoder and GMM clustering
+
+#from sklearn import mixture
+
+#gmm = mixture.GaussianMixture(10, covatiance_type='diag')
+
+
+# Here we define the structure of the VAE. We can pick and choose different
+# architectures to plug in as the encoder and decoder. The glueing together
+# of the encoder and decoder is done inside of the VAE class. This will need
+# to be the case for any new vae architectures that we code up, i.e. VaDE.
+input_dim = (28,28)
+encoder = DNN([500,500,2000], tf.nn.elu)
+latency_dim = 10
+decoder = DNN([2000,500,500], tf.nn.elu)
+hyperParams = {'reconstruct_cost': 'bernoulli',
+               'learning_rate': 1e-3,
+               'optimizer': tf.train.AdamOptimizer,
+               'batch_size': 100,
+               'alpha': 1.0,
+               'num_clusters': 10,
+               'prior': 'gaussian'}
+
+
+network = model(input_dim, encoder, latency_dim, decoder, hyperParams)
+
+itrs_per_epoch = mnist.train.num_examples // hyperParams['batch_size']
+epochs = 100
+
+test_data, test_labels = mnist.train.next_batch(hyperParams['batch_size'])
+for itr in tqdm(range(epochs*itrs_per_epoch)):
+    train_data, train_labels = mnist.train.next_batch(hyperParams['batch_size'])
+    tot_cost, reconstr_loss, KL_loss = network(train_data)
+
+test_data, test_labels = mnist.test.next_batch(200)
+labels = []
+for tl in test_labels:
+    labels.append(np.where(tl)[0][0])
+#network.create_embedding(test_data, (28,28), np.where(test_labels)[1])
+
+means, stds = network.transform(test_data)
+latent_vecs = means + np.random.normal(size=stds.shape)*stds
+reconstructions = network.reconstruct(test_data)
+fig = plt.figure(0)
+numFigs = 5
+for img in range(numFigs):
+    axes = fig.add_subplot(numFigs,2,2*img+1)
+    axes.imshow(test_data[img].reshape(28,28))
+    axes = fig.add_subplot(numFigs,2,2*img+2)
+    axes.imshow(reconstructions[img].reshape(28,28))
+
+counter = 1
+for perp in tqdm([20,25,30,40]):
+    mappings = TSNE(n_components=2, perplexity=perp).fit_transform(latent_vecs)
+    plt.figure(counter)
+    plt.title("Representation with Perplexity = %d" % (perp))
+    plt.scatter(mappings[:,0], mappings[:,1], c=labels)
+    counter += 1
+
+
+#plt.tight_layout()
+plt.show()
